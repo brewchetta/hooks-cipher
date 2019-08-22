caa3fe5b56fef2f7286e5290ceb22006923012e8
Chett Tiller <chett.tiller@gmail.com>
2019-08-22 11:40:20 -0400

    Set up solution branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// React (don't forget to import our new hooks)
-import React from 'react';
+// React
+import React, {useState, useEffect} from 'react';
 // Ciphers
 import {caesarsCipher, backwardsCipher} from './ciphers'
 // Styling
@@ -7,31 +7,57 @@ import './App.css';
 
 function App() {
 
-  /* Use hooks here to set the state for the current input */
+  const [cipher, setCipher] = useState('none')
+  const [message, setMessage] = useState('')
+  const [encoded, setEncoded] = useState(false)
 
+  /* Instructors: Build out the controlled form for the cipher selector */
   const handleSelectorChange = event => {
-    // Set state for current cipher here
+    setCipher(event.target.value)
   }
 
+  /* Instructors: Build something like this to show useEffect */
+  useEffect(() => {
+    console.log(`Your cipher is now ${cipher}`)
+    return () => console.log('Cleaning up')
+  }, [cipher])
+
+  /* Instructors: Once selector has been controlled, have students build out the rest of the app */
+
   const handleInputChange = event => {
-    // Set state for form input here
+    setMessage(event.target.value)
   }
 
   const handleSubmit = event => {
     event.preventDefault()
-    // Somewhere in here we need to update state which sets off a countdown timer
+
+    if (cipher === 'caesarsCipher') {
+      setMessage( caesarsCipher(message) )
+    } else if (cipher === 'backwardsCipher') {
+      setMessage( backwardsCipher(message) )
+    }
+
+    setEncoded(true)
   }
 
-  /* Import useEffect and think carefully */
-  /* How will it only fire when certain pieces of state update? */
+  useEffect(() => {
+
+    if (encoded) {
+      const tickTock = setTimeout(() => {
+        setMessage('')
+        setEncoded(false)
+      }, 5000)
+      return () => clearTimeout(tickTock)
+    }
+
+  }, [encoded])
 
   return (
     <div className="App">
 
       <h1>Top Secret: For Your Eyes Only</h1>
 
-      {/* Make this selector a controlled form using state and hooks, it determines  */}
-      <select onChange={handleSelectorChange} value='none'>
+      <select onChange={handleSelectorChange} value={cipher}>
         <option value='none'>---</option>
         <option value='caesarsCipher'>Caesar's Cipher</option>
         <option value='backwardsCipher'>Backwards Cipher</option>
@@ -39,10 +65,8 @@ function App() {
 
       <p>Super Secret Message:</p>
 
-      {/* Create a controlled form that holds the secret message in state and */}
-      {/* Once submitted, it encodes the message with the current cipher and begins a countdown to erase it */}
       <form onSubmit={handleSubmit}>
-        <input type='text' onChange={handleInputChange} value='' placeholder='Secrets go here' />
+        <input type='text' onChange={handleInputChange} value={message} placeholder='Secrets go here' />
         <input type='submit' value='Encode Message' />
       </form>
 
