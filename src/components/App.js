5ca77969f58ca3faa26e882456ff8bc79e30e52d
Chett Tiller <chett.tiller@gmail.com>
2019-08-22 10:52:18 -0400

    Simplified exercise, removed component hierarchy

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,47 @@
 // React
-import React from 'react';
-// Components
-import CipherSelector from './CipherSelector'
-import MessageCard from './MessageCard'
+import React, {useState} from 'react';
 // Styling
 import '../App.css';
 
 function App() {
 
-  /* Use hooks to declare your State here */
+  /* Use hooks here to set the state for the current input */
 
-  const chooseCurrentCipher = event => {
-
+  const handleSelectorChange = event => {
+    // Now that we have a hook for the current cipher, how do we set it here?
   }
+
+  const handleInputChange = event => {
+    // When we type in the message, we ought to hold it in state as well!
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    // Somewhere in here we update some state which sets off a countdown timer
+  }
+
+  /* Import useEffect and think carefully */
+  /* How will it only fire when certain pieces of state update? */
 
   return (
     <div className="App">
+
       <h1>Top Secret: For Your Eyes Only</h1>
 
-      {/* Pass down props as needed to the child components */}
+      {/* Make this selector a controlled form using state and hooks, it determines  */}
+      <select onChange={handleSelectorChange} value='none'>
+        <option value='none'>---</option>
+        <option value='caesarsCipher'>Caesar's Cipher</option>
+        <option value='backwardsCipher'>Backwards Cipher</option>
+      </select>
 
-      <CipherSelector chooseCurrentCipher={chooseCurrentCipher} />
+      <p>Super Secret Message:</p>
 
-      <MessageCard />
+      {/* Create a controlled form that holds the secret message in state and, once submitted, encodes the message and begins a countdown to erase it */}
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleInputChange} value='' placeholder='Secrets go here' />
+        <input type='submit' value='Decode Message' />
+      </form>
 
     </div>
   );
