ed2860cc391b0947975d34122af9132ccccabfd7
brewchetta <chett.tiller@gmail.com>
2020-01-06 16:28:02 -0500

    Updated comments and spacing for clarity

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
 // React (don't forget to import our new hooks)
-import React from 'react';
+import React from 'react'
 // Ciphers
 import {caesarsCipher, backwardsCipher} from './ciphers'
 // Styling
-import './App.css';
+import './App.css'
 
 function App() {
 
-  /* Use hooks here to set the state for the current input */
+  /* Use hooks here to set the state */
 
   const handleSelectorChange = event => {
-    // Set state for current cipher here
+    /* Set state for current cipher here */
   }
 
   const handleInputChange = event => {
-    // Set state for form input here
+    /* Set state for form input here */
   }
 
   const handleSubmit = event => {
     event.preventDefault()
-    // Somewhere in here we need to update state which sets off a countdown timer
+    /* Update state here */
   }
-
-  /* Import useEffect and think carefully */
-  /* How will it only fire when certain pieces of state update? */
 
   return (
     <div className="App">
 
       <h1>Top Secret: For Your Eyes Only</h1>
 
-      {/* Make this selector a controlled form using state and hooks, it determines  */}
-      <select onChange={handleSelectorChange} value='none'>
+      {/*------ Select Cipher ------*/}
+
+      <select
+      onChange={handleSelectorChange}
+      value='none'>
+
         <option value='none'>---</option>
         <option value='caesarsCipher'>Caesar's Cipher</option>
         <option value='backwardsCipher'>Backwards Cipher</option>
+
       </select>
+
+      {/*------ Message Input ------*/}
 
       <p>Super Secret Message:</p>
 
-      {/* Create a controlled form that holds the secret message in state and */}
-      {/* Once submitted, it encodes the message with the current cipher and begins a countdown to erase it */}
       <form onSubmit={handleSubmit}>
-        <input type='text' onChange={handleInputChange} value='' placeholder='Secrets go here' />
+
+        <input type='text'
+        onChange={handleInputChange}
+        value=''
+        placeholder='Secrets go here' />
+
         <input type='submit' value='Encode Message' />
+
       </form>
 
     </div>
